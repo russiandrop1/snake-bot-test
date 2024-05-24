d50ff77fc3c856439f03292fe0b0a830f6f288ec
OLEGATOR <143329761+russiandrop1@users.noreply.github.com>
2024-05-24 22:39:39 +0800

    bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ def start(update: Update, context: CallbackContext) -> None:
     update.message.reply_text('Press the button to play the game!', reply_markup=reply_markup)
 
 def main():
-    updater = Updater("6305004926:AAHSGnXyHsPO-ltLN0ZTdkeG9RwFYKR_fus", use_context=True)
+    updater = Updater("6305004926:AAHSGnXyHsPO-ltLN0ZTdkeG9RwFYKR_fus")
     updater.dispatcher.add_handler(CommandHandler("start", start))
     updater.start_polling()
     updater.idle()
