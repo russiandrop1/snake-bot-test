5f55c69a91d70fa92cfd2f20868104bca6636f33
OLEGATOR <143329761+russiandrop1@users.noreply.github.com>
2024-05-24 22:47:24 +0800

    bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, WebAppInfo
-from telegram.ext import Updater, CommandHandler, CallbackContext
+from telegram.ext import Application, CommandHandler, CallbackContext
 
 def start(update: Update, context: CallbackContext) -> None:
     keyboard = [
@@ -9,10 +9,9 @@ def start(update: Update, context: CallbackContext) -> None:
     update.message.reply_text('Press the button to play the game!', reply_markup=reply_markup)
 
 def main():
-    updater = Updater("6305004926:AAHSGnXyHsPO-ltLN0ZTdkeG9RwFYKR_fus")
-    updater.dispatcher.add_handler(CommandHandler("start", start))
-    updater.start_polling()
-    updater.idle()
+    application = Application.builder().token("6305004926:AAHSGnXyHsPO-ltLN0ZTdkeG9RwFYKR_fus").build()
+    application.add_handler(CommandHandler("start", start))
+    application.run_polling()
 
 if __name__ == '__main__':
     main()
